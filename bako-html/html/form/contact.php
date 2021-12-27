9621b36746555aae6a5009e97e859b1bb13036a2
Lee Maurin <leem4421@gmail.com>
2021-12-26 20:08:58 -0600

    add email add formsubmit

--- bako-html/html/form/contact.php
+++ bako-html/html/form/contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <YOUR_EMAIL@DOMAIN.COM>';
+$from = 'Demo contact form <LEEM4421@GMAIL.COM>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'Demo contact form <LEEM4421@GMAIL.COM>';
